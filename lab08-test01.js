df3b5132e1a230891cb0b77d80da33db16a6cb36
dshooby <152923174+dshooby@users.noreply.github.com>
2024-02-29 21:14:19 -0500

    classwork 6b

--- lab08-test01.js
+++ lab08-test01.js
@@ -1,15 +1,24 @@
 /* add code below this */
 
-let bill = prompt("What is the billed total?");
-let iBill = Number(bill); //parses user input to number
+const billTotals = [50., 150.00, 20.00, 500.00];
+const tips = [];
 
-const TIP_PERCENTAGE = 0.10;
-let tip = iBill * TIP_PERCENTAGE;
+for(let total of billTotals){
+    let tip;
+    if(total < 30){
+        tip = total * 0.30;
+    }
+    else if (total >=30 && total <= 75)
+        tip = total * 0.20;
+    else {
+        tip = total * 0.10;
+    }
 
-if (iBill !=undefined && iBill !=0)
-{
-console.log("For bill of $ "+bill+" the tip should be $ "+tip);
-console.log(`For bill of $${bill} the tip should be $ ${tip}`);
-} else {
-    console.log(`${bill}  is not valid number`);
+    //tip = total < 30 ? total * 0.30 : total * 0.10;
+    tips.push(tip);
+
+}
+
+for (let i=0; i < billTotals.length; i++) {
+    console.log("For bill of $ " + billTotals[i] + " the tip should be $" + tips[i]);
 }
