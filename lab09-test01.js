28a5e4049f67d588864bdd80b37f546bddb523c0
dshooby <152923174+dshooby@users.noreply.github.com>
2024-03-07 21:46:09 -0500

    classwork 7b

---
+++ lab09-test01.js
@@ -0,0 +1,19 @@
+/* add code after this comment */
+
+console.log(document.getElementById("thumb-list"));
+console.log(`MANY: `)
+console.log(document.getElementsByClassName("thumb"));
+console.log(`FIRST: `)
+console.log(document.getElementsByClassName("thumb")[0]);
+
+document.getElementById('thumb-list').style.border = "solid 1px #CCCCCC"
+document.querySelector('#msg').value = document.querySelector('.box fieldset p').textContent;
+
+const images = document.querySelectorAll('#thum-list img');
+console.log(images);
+for (let img of images) {
+    img.style.boxShadow = "6px 5px 1px rgba(0,0,0,.22)"
+};
+
+const d = document.querySelector('#msg');
+d.innerHTML = '<h2>HELLO WORLD</h2>'
