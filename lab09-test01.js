1ba56e21e6d457333a1715b29d2d92bd66497ca6
dshooby <152923174+dshooby@users.noreply.github.com>
2024-03-13 22:13:17 -0400

    classwork 7b take 2

--- lab09-test01.js
+++ lab09-test01.js
@@ -9,7 +9,7 @@ console.log(document.getElementsByClassName("thumb")[0]);
 document.getElementById('thumb-list').style.border = "solid 1px #CCCCCC"
 document.querySelector('#msg').value = document.querySelector('.box fieldset p').textContent;
 
-const images = document.querySelectorAll('#thum-list img');
+const images = document.querySelectorAll('#thumb-list img');
 console.log(images);
 for (let img of images) {
     img.style.boxShadow = "6px 5px 1px rgba(0,0,0,.22)"
